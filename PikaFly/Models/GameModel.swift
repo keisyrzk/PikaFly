510cb635484328bd626201b41740874dbf681c9d
Esteban <krisstoferos@gmail.com>
2018-07-26 12:52:21 +0200

    launcher update

--- PikaFly/Models/GameModel.swift
+++ PikaFly/Models/GameModel.swift
@@ -22,11 +22,11 @@ class GameModel {
     var power: CGFloat = 0
     
     func getRadians(from angle: CGFloat) -> CGFloat {
-        return angle * 3.14 / 180
+        return angle * CGFloat.pi / 180
     }
     
     func getAngle(from radians: CGFloat) {
-        launchAngel = radians * 180 / 3.14
+        launchAngel = radians
     }
     
     func getPower(nodeFrame: CGRect) {
